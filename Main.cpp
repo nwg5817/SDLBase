c07c22193c47f91225e74eab6e6065006ddd6760
nwg5817 <nwg5817@gmail.com>
2020-08-08 17:15:07 -0400

    init

---
+++ Main.cpp
@@ -0,0 +1,52 @@
+// SDLBase.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "SDL2/SDL.h"
+#include "Game.h"
+
+Game *game = nullptr;
+
+int main(int argc, char *argv[])
+{
+
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
+	game = new Game();
+	game->init("GameWindow", 800, 600, false);
+
+	while (game->running())
+	{
+
+		frameStart = SDL_GetTicks();
+
+		game->handleEvents();
+		game->update();
+		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
+	}
+
+	game->clean();
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
